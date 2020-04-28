a9f64671b128fb959955ed4c9c017b4229b8aa52
daviidy <yaodavidarmel@gmail.com>
2020-04-28 20:11:46 +0000

    studio plan page 4

--- resources/views/studioPlan.blade.php
+++ resources/views/studioPlan.blade.php
@@ -69,7 +69,7 @@ transition: all 0.2s;
                   <div class="col-4">
                       <h6>Mon Studio'Plan</h6>
                   </div>
-                  <div class="col-6 col-sm-6 col-md-3">
+                  <div class="col-12 col-sm-12 col-md-3">
                       <h6>Temps restant</h6>
                       <div class="row">
                           <div class="col-2">

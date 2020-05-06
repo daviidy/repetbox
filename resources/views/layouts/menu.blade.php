042043acbc7ec6df00b56f484ad6336ff22da53e
daviidy <yaodavidarmel@gmail.com>
2020-05-06 01:17:48 +0000

    migrations issue 6

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -393,9 +393,11 @@ a[_ngcontent-serverApp-c11]:hover   .nav-dropdown__profile[_ngcontent-serverApp-
                 <li _ngcontent-serverapp-c9="" class="sidetray__item">
                   <!---->
                   <!----><a _ngcontent-serverapp-c9="" class="sidetray__link ng-star-inserted" data-qa="sidetray-home" routerlink="/" href="/">Accueil</a></li>
+                 @auth
                 <li _ngcontent-serverapp-c9="" class="sidetray__item"><a _ngcontent-serverapp-c9="" class="sidetray__link" data-qa="sidetray-studio" routerlink="/studio" routerlinkactive="is-active" href="/{{Auth::user()->id}}/recordings/create">Créer un enregistrement</a></li>
                 <li _ngcontent-serverapp-c9="" class="sidetray__item"><a _ngcontent-serverapp-c9="" class="sidetray__link" data-qa="sidetray-community" routerlink="/explore/contests" routerlinkactive="is-active" href="#">Rechercher des musiciens</a>
                 </li>
+                @endauth
 								<li _ngcontent-serverapp-c9="" class="sidetray__item"><a _ngcontent-serverapp-c9="" class="sidetray__link" data-qa="sidetray-community" routerlink="/explore/contests" routerlinkactive="is-active" href="/studioPlan">Mon Studio'Plan</a>
                 </li>
                 <li _ngcontent-serverapp-c9="" class="sidetray__item"><a _ngcontent-serverapp-c9="" class="sidetray__link" data-qa="sidetray-sounds" routerlink="/sounds" routerlinkactive="is-active" href="#">Utiliser Studio'Box</a></li>
